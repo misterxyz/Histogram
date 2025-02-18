866d342dd6a630f19b33fdbc6e359b357b842d9a
misterxyz <mister_vladimirov@yahoo.de>
2025-02-18 21:43:11 +0100

    Zach_4.py

--- Zach_1.py
+++ Zach_1.py
@@ -26,4 +26,15 @@ def vec_op_euk (vec_a):                        #Euklidische Norm
     ret=(vec_a[0]*vec_a[0]+vec_a[1]*vec_a[1]+vec_a[2]*vec_a[2])**0.5  #**0.5 bedeutet Wurzel
     return ret
 
-print(vec_op_euk([2,3,4]))
+# Berechnet das Kreuzprodukt von 2 Vektoren
+# vec_a     Array[number, number, number]   1.Vektor  
+# vec_b     Array[number, number, number]   2.Vektor 
+# vec_c     Array[number, number, number]   Kreuzprodukt der beiden Vektoren 
+def vec_op_cross(vec_a, vec_b):             #Kreuzprodukt
+    vec_c = [0,0,0]
+    vec_c[0] = vec_a[1]*vec_b[2]-vec_a[2]*vec_b[1]
+    vec_c[1] = vec_a[2]*vec_b[0]-vec_a[0]*vec_b[2]
+    vec_c[2] = vec_a[0]*vec_b[1]-vec_a[1]*vec_b[0]
+    return vec_c
+
+print(vec_op_cross([1,2,3], [4,5,6]))
