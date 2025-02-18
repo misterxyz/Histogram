863860802adc7a5897f3dccc29c17341c17d47ce
misterxyz <mister_vladimirov@yahoo.de>
2025-02-18 21:41:09 +0100

    Zach_2.py

--- Zach_1.py
+++ Zach_1.py
@@ -11,4 +11,12 @@ def vec_op_minus(vec_a, vec_b):                 #Vektorsubtraktion
     vec_c[2] = vec_a[2]-vec_b[2]
     return vec_c
 
-print(vec_op_minus([9,8,7], [5,3,4]))
+# Berechnet das Skalarprodukt von 2 Vektoren
+# vec_a         Array[number, number, number]   1.Vektor  
+# vec_b         Array[number, number, number]   2.Vektor 
+# return ret    Ergebnis[number]                Skalarprodukt der beiden Vektoren 
+def vec_op_sp (vec_a, vec_b):                  #Skalarprodukt
+    ret = vec_a[0]*vec_b[0]+vec_a[1]*vec_b[1]+vec_a[2]*vec_b[2]
+    return ret
+
+print(vec_op_sp ([1,2,3], [4,5,6]))
